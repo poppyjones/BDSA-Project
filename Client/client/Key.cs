8f6ce637c1b3ce81a37e66be26a3dd84df28a321
Isabella <isam@itu.dk>
2021-12-02 15:50:01 +0100

    CreatePost is updated

--- Client/client/Key.cs
+++
@@ -1,9 +0,0 @@
-public enum Key
-{
-    AI,
-    Python,
-    MachineLearning,
-    CSharp
-
-
-}
--- Client/client/Keyword.cs
+++
@@ -1,8 +0,0 @@
-public class keywords
-{
-    public int KeywordId { get; set; }
-
-    public string KeywordName { get; set; }
-
-    public Key SuggestedKeywords { get; set; }
-}
