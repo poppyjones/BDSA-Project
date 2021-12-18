9c635fbfffbec2f09e19fbb309aeb5384a3f2d3f
Isabella <isam@itu.dk>
2021-12-18 14:08:02 +0100

    authorization works

--- Server/server/Program.cs
+++ Server/server/Program.cs
@@ -1,62 +1,21 @@
 using Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace main
 {
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
-
-
-
-namespace main {
-    public class Program {
+    public class Program 
+    {
         public static void Main(string[] args)
         {
-            // LoadAuthentication();
             var configuration = LoadConfiguration();
             var connectionString = configuration.GetConnectionString("PrimeSlice");
 
             var optionsBuilder = new DbContextOptionsBuilder<Context>().UseSqlServer(connectionString);
             using var context = new Context(optionsBuilder.Options);
             ContextFactory.Seed(context);
-        }
-
-        public static void LoadAuthentication()
-        {
+            //LoadAuthentication();
+            
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -97,6 +56,49 @@ namespace main {
 
             app.Run();
         }
+
+        /* public static void LoadAuthentication()
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapRazorPages();
+            app.MapControllers();
+            app.MapFallbackToFile("index.html");
+
+            app.Run();
+        } */
 
         static IConfiguration LoadConfiguration()
         {
