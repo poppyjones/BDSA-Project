887a3b321bc62f1c5ce0fbbcf238200481f72993
poppyjones <poppy.em.jones@gmail.com>
2021-12-20 17:40:26 +0100

    post api works

--- Server/Controllers/PostController.cs
+++ Server/Controllers/PostController.cs
@@ -4,27 +4,35 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Web.Http;
 using server.Model;
+using server.Repositories;
+using server.Interfaces;
+
 
 namespace server.Controllers
 {
-
+    
     [ApiController]
     [Route("[controller]")]
 
     public class PostController : ApiController
     {
-        //private readonly ILogger<PostController> _logger;
+        private readonly ILogger<PostController> _logger;
+        private readonly IPostRepository _repository;
 
-        public PostController()//ILogger<PostController> logger)
+        public PostController(ILogger<PostController> logger, IPostRepository repository)
         {
-            //_logger = logger;
+            _logger = logger;
+            _repository = repository;
         }
 
-        [EnableCors]
+       // [EnableCors]
         [HttpGet]
         public IEnumerable<PostDTO> Get()
         {
-            throw new NotImplementedException();
+            return _repository.ReadAllByAuthorId(1);
+
+
+            //throw new NotImplementedException();
             // return Enumerable.Range(1, 1).Select(index => new PostDTO
             // {
             //     Id = 1,
