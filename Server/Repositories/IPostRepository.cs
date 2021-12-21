dd318de553bfe35cb0596a2e2bdae57043378079
poppyjones <poppy.em.jones@gmail.com>
2021-12-21 19:15:13 +0100

    rettet lille bitte fejl

--- Server/Repositories/IPostRepository.cs
+++ Server/Repositories/IPostRepository.cs
@@ -7,6 +7,6 @@ public interface IPostRepository
 {
     int Create(PostCreateDTO post);
     PostDTO ReadByPostId(int PostId);
-    ICollection<PostDTO> ReadAllByAuthorId(int PostId);
+    ICollection<PostDTO> ReadAllByAuthorId(int AuthorId);
 }
 
